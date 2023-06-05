d00ac992bfee6d1c09f8fdef255eb0a940a31c93
Lucheto04 <lucho.lgrg04@gmail.com>
2023-06-05 12:38:33 -0500

    feat: :sparkles: Terminamos el taller 9.
    Terminamos de eliminar los valores duplicados en el array de 'aliens'.

---
+++ Talleres/taller-9.php
@@ -0,0 +1,19 @@
+<?php 
+    $aliens = array(
+        "Greys",
+        "Reptilians",
+        "Insectoids",
+        "Reptilians",
+        "Energy Beings",
+        "Greys",
+        "Shape-shifters",
+        "Shape-shifters",
+        "Greys",
+        "Insectoids"
+    );
+
+    print_r(array_unique($aliens));
+
+    echo "<hr>";
+    echo '<a href="index.html">Volver</a>';
+?>
