59590573946291d4d4635d0b4290f7da7bfd32f5
Lucheto04 <lucho.lgrg04@gmail.com>
2023-06-05 11:57:31 -0500

    feat: :sparkles: Terminamos el taller 6
    Terminamos el buscador de planetas para confirmar si estan dentro del sistema solar, ejercicio del taller 6.

---
+++ Talleres/taller-6.php
@@ -0,0 +1,21 @@
+<?php 
+   $planetas = array(
+    "Sun",
+    "Mercury",
+    "Venus",
+    "Earth",
+    "Mars",
+    "Jupiter",
+    "Saturn",
+    "Uranus",
+    "Neptune"
+    );
+    $busqueda = (string) $_GET['planeta'];
+    if (in_array($busqueda, $planetas, true)) {
+        echo "El planeta $busqueda si esta dentro del sistema solar";
+    } else {
+        echo "El planeta $busqueda no esta dentro del sistema solar";
+    };
+    echo "<hr>";
+    echo '<a href="index.html">Volver</a>';
+?>
