a8fe1a6bfbc2f09d41cdf77478a85e32cd1d2bcb
Lucheto04 <lucho.lgrg04@gmail.com>
2023-06-05 13:06:20 -0500

    feat: :sparkles: Añadimos el taller 12.
    terminamos el formulario del taller 12, ya solo noes queda hacer el php para que tenga la funcionalidad solicitada.

---
+++ Talleres/taller-12.php
@@ -0,0 +1,18 @@
+<?php 
+    $satelites = array(
+        "Earth" => array(
+            ),
+        "Mars" => array(
+            ),
+        "Jupiter" => array(
+            ),
+        "Saturn" => array(
+            ),
+        "Uranus" => array(
+            ),
+    );
+
+
+    echo "<hr>";
+    echo '<a href="index.html">Volver</a>';
+?>
