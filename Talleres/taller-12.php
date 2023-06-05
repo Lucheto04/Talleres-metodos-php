1c5b0d79d6f5fcfed39e0573eb243276d921ab63
lucho <lucho.lgrg04@gmail.com>
2023-06-05 14:49:11 -0500

    build: :construction: Cambios en los botones del taller 12
    Conseguimos el tanto el input text como el boton de Agregar no me manden al archivo php si no que solo me guarden el dato para que solo el boton finalizar me muestre los datos que quiero del archivo php

--- Talleres/taller-12.php
+++ Talleres/taller-12.php
@@ -1,18 +1,30 @@
 <?php 
+    session_start();
     $satelites = array(
-        "Earth" => array(
-            ),
-        "Mars" => array(
-            ),
-        "Jupiter" => array(
-            ),
-        "Saturn" => array(
-            ),
-        "Uranus" => array(
-            ),
+        "Earth" => array(),
+        "Mars" =>array(),
+        "Jupiter" => array(),
+        "Saturn" => array(),
+        "Uranus" => array(),
     );
+
+    $planeta = $_POST['planetas'];
+
+    $satelite = $_POST['satelite'];
+
+    $_SESSION['sat'] = $satelites;
+
+    print_r($_SESSION);
+    // print_r($planeta);
+    
+    // Narray_push($satelites[$planeta], $satelite);
 
 
     echo "<hr>";
     echo '<a href="index.html">Volver</a>';
+
+    if ($_POST['agregar']) {
+        $pagina = $_SERVER["HTTP_REFERER"]."#taller-12";
+        header("Refresh: 0;url= $pagina");
+    };
 ?>
