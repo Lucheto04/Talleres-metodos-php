21d75dcbefb8eba52c09c7fea7b2a8bfb6060b68
Lucheto04 <lucho.lgrg04@gmail.com>
2023-06-05 12:54:21 -0500

    feat: :sparkles: Terminamos el taller 13.
    Terminamos el taller 13 primero que el 12 porque se me acaba el tiempo y tengo que ir a almorzar, y vi que el 13 era muy facil de hacer y salia mas rapido.

---
+++ Talleres/taller-13.php
@@ -0,0 +1,20 @@
+<?php 
+   $planetas = array(
+    "Sun",
+    "Mercury",
+    "Venus",
+    "Earth",
+    "Mars",
+    "Jupiter",
+    "Saturn",
+    "Uranus",
+    "Neptune"
+    );
+
+
+    print_r($planetas); echo "<br><br>";
+    print_r(array_reverse($planetas));
+
+    echo "<hr>";
+    echo '<a href="index.html">Volver</a>';
+?>
